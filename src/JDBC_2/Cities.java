676534cfa0cfae0ba82a21460c4f9bc2c1cddaab
taev03 <94910314+taev03@users.noreply.github.com>
2022-04-30 21:36:01 +0600

    init

---
+++ src/JDBC_2/Cities.java
@@ -0,0 +1,38 @@
+package JDBC_2;
+
+public class Cities {
+    int id;
+    String name;
+
+    public Cities(){
+
+    }
+    public Cities (int id, String name){
+        this. id = id;
+        this. name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Cities{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/JDBC_2/Main.java
@@ -0,0 +1,19 @@
+package JDBC_2;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private static final String url = "jdbc:postgresql://localhost:5432/";
+    private static final String user = "postgres";
+    private static final String password = "master";
+    public static void main(String[] args) {
+
+    }
+    private List<Cities> getAllCities (){
+        List <Cities> cities = new ArrayList<>();
+
+        return cities;
+    }
+}
